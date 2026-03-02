bf26ebc6bce6b11aadd337f68a9392f032127c00
ShivamP-97 <shivampathak19972@gmail.com>
2026-03-02 22:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,79 +1,21 @@
-const statusDiv = document.getElementById("status");
-const messageInput = document.getElementById("message-input");
-const sendButton = document.getElementById("send-button");
-const disconnectButton = document.getElementById("disconnect-button");
-const notificationsDiv = document.getElementById("notifications");
+// This file is not to be modified. Please ignore this.
+// We will understand all of this later in the course.
+// DO NOT MODIFY THIS FILE
 
-const wsUrl = "wss://socketsbay.com/wss/v2/1/demo/";
-let socket = null;
-let reconnectTimer = null;
+const express = require('express');
+const path = require('path');
 
-const displayNotification = (message) => {
-  const div = document.createElement("div");
-  div.textContent = message;
-  notificationsDiv.appendChild(div);
-};
+const app = express();
 
-const connectWebSocket = () => {
-  statusDiv.textContent = "Connecting...";
-  socket = new WebSocket(wsUrl);
+app.use(express.static(__dirname))
 
-  socket.addEventListener("open", () => {
-    statusDiv.textContent = "Connected";
-
-    messageInput.disabled = false;
-    sendButton.disabled = false;
-    disconnectButton.disabled = false;
-
-    displayNotification("Connected to server");
-
-    if (reconnectTimer) {
-      clearTimeout(reconnectTimer);
-      reconnectTimer = null;
-    }
-  });
-
-  socket.addEventListener("message", (event) => {
-    displayNotification(`Notification: ${event.data}`);
-  });
-
-  socket.addEventListener("error", () => {
-    console.log("WebSocket error");
-  });
-
-  socket.addEventListener("close", () => {
-    statusDiv.textContent = "Disconnected";
-
-    messageInput.disabled = true;
-    sendButton.disabled = true;
-    disconnectButton.disabled = true;
-
-    displayNotification("Disconnected from server");
-
-    if (!reconnectTimer) {
-      reconnectTimer = setTimeout(() => {
-        displayNotification("Attempting to reconnect...");
-        connectWebSocket();
-      }, 10000);
-    }
-  });
-};
-
-sendButton.addEventListener("click", () => {
-  const msg = messageInput.value.trim();
-  if (!msg) return;
-
-  if (socket && socket.readyState === WebSocket.OPEN) {
-    socket.send(msg);
-    displayNotification(`You: ${msg}`);
-    messageInput.value = "";
-  }
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
 });
-
-disconnectButton.addEventListener("click", () => {
-  if (socket && socket.readyState === WebSocket.OPEN) {
-    socket.close();
-  }
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
 });
-
-connectWebSocket();
+module.exports = app;
